02d7311ff71fe351c0c04d3b6ba800a830842a93
ViViV_ <sysadmin@whiteshield.ch>
2023-07-06 16:34:55 +0200

    Fonction Mod
    Modification de la fonction DemanderNombre pour inclure des paramentre Min et Max:

--- C#_Project/Magic_Number/Magic_Number/Program.cs
+++ C#_Project/Magic_Number/Magic_Number/Program.cs
@@ -1,14 +1,49 @@
 ﻿
 using System;
-
+using System.Linq.Expressions;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Magic_Number
 {
     class Program
     {
+        static int DemanderNombre(int min, int max)
+        {
+            int nbrs = min -1;
+            while ((nbrs < min) || (nbrs > max))
+            {
+                Console.WriteLine("Veuillez entrer un nombre compris entre " + min + " et " + max);
+                string nbrs_str = Console.ReadLine();
+
+
+                try
+                {
+                    nbrs = int.Parse(nbrs_str);
+
+                    if ((nbrs < min) || (nbrs > max))
+                    {
+                        Console.WriteLine("le nombres doit etre comris entre " + min + " et " + max + ":");
+                    }
+                    
+                    
+                }
+                catch
+                {
+                    Console.WriteLine("Erreur vous devez entrer un nombre valide.");
+                }
+            }
+            return nbrs;        
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            const int NBRS_MIN = 1;
+            const int NBRS_MAX = 10;
+
+            int nbrs = DemanderNombre(NBRS_MIN, NBRS_MAX);
+            Console.WriteLine("Le nombres qu vous avez choisi est: " + nbrs);
         }
     }
 }
