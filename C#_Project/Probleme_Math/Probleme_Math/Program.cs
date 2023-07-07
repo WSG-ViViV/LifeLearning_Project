57b3ba58b5e66789216a2c2fa89dda874643eadc
ViViV_ <sysadmin@whiteshield.ch>
2023-07-07 23:30:37 +0200

    PoserQuestion()
    Implémentation de la fonction
    
    Ajout d'une boucle de contrôle pour la validité du nombre entré
    catch de l'erreur si l'entrée utilisateur est invalide

--- C#_Project/Probleme_Math/Probleme_Math/Program.cs
+++ C#_Project/Probleme_Math/Probleme_Math/Program.cs
@@ -4,9 +4,31 @@ namespace jeu_de_maths
 { 
     class Program
     {
+
+        static void PoserQuestion()
+        {
+            int reponseInt = 0;
+            while (true)
+            {
+                Console.Write("5 + 2 = ");
+                string reponse = Console.ReadLine();
+                try
+                {
+                    reponseInt = int.Parse(reponse);
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("ERREUR: Vous devez entre un nombre valide !!");
+                }
+            }
+           
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            PoserQuestion();
         }
         
     }
