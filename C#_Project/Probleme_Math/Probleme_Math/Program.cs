7b2505c62da489ab01c16179ebdac370c69b8ba3
ViViV_ <sysadmin@whiteshield.ch>
2023-07-07 23:18:14 +0200

    Init jeu_de_maths
    start boilerplate

---
+++ C#_Project/Probleme_Math/Probleme_Math/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace jeu_de_maths
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        
+    }
+
+}
