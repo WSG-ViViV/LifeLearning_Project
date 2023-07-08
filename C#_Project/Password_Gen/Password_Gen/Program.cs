94ac06d7d8903471199220b6c8ea123ec9fe2afb
ViViV_ <sysadmin@whiteshield.ch>
2023-07-08 23:07:15 +0200

    init project

---
+++ C#_Project/Password_Gen/Password_Gen/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace Password_Gen
+{
+
+    class Program
+    {
+
+        static int DemanderNombre(string question)
+        { 
+            while (true)
+            {
+                Console.WriteLine(question);
+                string longeurPass = Console.ReadLine();
+                try
+                {
+                   int longeurPassInt = int.Parse(longeurPass);
+                    if (longeurPassInt <= 0)
+                    {
+                        Console.WriteLine("Le nombre ne peux pas etre negatif !");
+                    }
+                    else
+                    {
+                        return longeurPassInt;
+                    }
+                    
+                }
+                catch
+                {
+                    Console.WriteLine("ERREUR: Veuillez entrer un nombre valide!!");
+                } 
+            }
+        }
+
+        static void Main(string[] args)
+        {
+             int longeurPassword = DemanderNombre("Longeur du mot de passe ?");
+        }
+    }
+}
+
+
